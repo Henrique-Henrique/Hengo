5910bbcf247b99c8657c32479edcfbc3bb4c4a0a
Henrique-Henrique <sr.hudrick@gmail.com>
2026-08-25 16:18:48 -0300

    fix - change color

--- addons/hengo/actions/render/change_color.gd
+++ addons/hengo/actions/render/change_color.gd
@@ -41,6 +41,7 @@ func get_flow_inputs() -> Array[Dictionary]:
 	return [
 		{name = 'Enter', id = &'enter'},
 		{name = 'Update', id = &'update'},
+		{name = 'Physics', id = &'physics'},
 		{name = 'Exit', id = &'exit'}
 	]
 
@@ -50,6 +51,10 @@ func get_flow_enter() -> String:
 
 
 func get_flow_update() -> String:
+	return _get_body()
+
+
+func get_flow_physics() -> String:
 	return _get_body()
 
 
